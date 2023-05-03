a1da6ac4bfb4ae94259afee2ef4fc61a29b5420d
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-05-03 15:31:50 +0400

    update settings

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-4978(v!3u*6q1qnsekkw)i-yfri9b2wgalzapb#k6zv5phgx1d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,13 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
+    os.path.join(BASE_DIR, 'static')
 ]
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-efca0.up.railway.app/']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
