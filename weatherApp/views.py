40ff0cece1d0d9371965ca0b86354da2d32fd673
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-05-04 11:01:28 +0400

    update the index and fetch_weather_data to catch errors

--- weatherApp/views.py
+++ weatherApp/views.py
@@ -2,8 +2,6 @@ import os
 from django.shortcuts import render
 import requests
 import datetime
-
-from django.views.decorators.csrf import ensure_csrf_cookie
 from dotenv import load_dotenv
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
 
@@ -20,36 +18,46 @@ def index(request):
         city = request.POST['city']
         weather_data, daily_forecasts = fetch_weather_data(city, api_key, current_weather_url, forecast_url)
 
-        context = {
-            'weather_data': weather_data,
-            'daily_forecasts': daily_forecasts,
-        }
-
-        return render(request, 'index.html', context)
+        if weather_data is None or daily_forecasts is None:
+            error_message = f"Sorry, we couldn't find the weather data for {city}. Please try again with a valid location."
+            context = {
+                'error_message': error_message,
+            }
+            return render(request, 'index.html', context)
+        else:
+            context = {
+                'weather_data': weather_data,
+                'daily_forecasts': daily_forecasts,
+            }
+            return render(request, 'index.html', context)
     else:
         return render(request, 'index.html')
 
 
+
 def fetch_weather_data(city, api_key, current_weather_url, forecast_url):
-    response = requests.get(current_weather_url.format(city, api_key)).json()
-    lat, lon = response['coord']['lat'], response['coord']['lon']
-    forecast_response = requests.get(forecast_url.format(lat, lon, api_key)).json()
+    try:
+        response = requests.get(current_weather_url.format(city, api_key)).json()
+        lat, lon = response['coord']['lat'], response['coord']['lon']
+        forecast_response = requests.get(forecast_url.format(lat, lon, api_key)).json()
 
-    weather_data = {
-        'city': city,
-        'temperature': round(response['main']['temp'] - 273.15, 2),
-        'description': response['weather'][0]['description'],
-        'icon': response['weather'][0]['icon'],
-    }
+        weather_data = {
+            'city': city,
+            'temperature': round(response['main']['temp'] - 273.15, 2),
+            'description': response['weather'][0]['description'],
+            'icon': response['weather'][0]['icon'],
+        }
 
-    daily_forecasts = []
-    for daily_data in forecast_response['daily'][:5]:
-        daily_forecasts.append({
-            'day': datetime.datetime.fromtimestamp(daily_data['dt']).strftime('%A'),
-            'min_temp': round(daily_data['temp']['min'] - 273.15, 2),
-            'max_temp': round(daily_data['temp']['max'] - 273.15, 2),
-            'description': daily_data['weather'][0]['description'],
-            'icon': daily_data['weather'][0]['icon'],
-        })
+        daily_forecasts = []
+        for daily_data in forecast_response['daily'][:5]:
+            daily_forecasts.append({
+                'day': datetime.datetime.fromtimestamp(daily_data['dt']).strftime('%A'),
+                'min_temp': round(daily_data['temp']['min'] - 273.15, 2),
+                'max_temp': round(daily_data['temp']['max'] - 273.15, 2),
+                'description': daily_data['weather'][0]['description'],
+                'icon': daily_data['weather'][0]['icon'],
+            })
 
-    return weather_data, daily_forecasts
+        return weather_data, daily_forecasts
+    except:
+        return None, None
